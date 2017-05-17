53f25f6c512601c00d5d04e26ce2cbd4aaaa278c
goncalo <github@goncalopereira.com>
2017-05-17 17:45:54 +0100

    remove lines in one liners in fizzbuzzvalue

--- src/main/scala/values/FizzBuzzValue.scala
+++ src/main/scala/values/FizzBuzzValue.scala
@@ -4,17 +4,17 @@ sealed abstract class FizzBuzzValue {
   def toValue(): String
 }
 final case class FizzBuzzInteger(i: Integer) extends FizzBuzzValue {
-  def toValue(): String = { i.toString() }
+  def toValue(): String = i.toString()
 }
 case object FizzString extends FizzBuzzValue {
-  def toValue(): String = { "fizz" }
+  def toValue(): String = "fizz"
 }
 case object BuzzString extends FizzBuzzValue {
-  def toValue(): String = { "buzz" }
+  def toValue(): String = "buzz"
 }
 case object FizzBuzzString extends FizzBuzzValue {
-  def toValue(): String = { "fizzbuzz" }
+  def toValue(): String = "fizzbuzz"
 }
 case object LuckyString extends FizzBuzzValue {
-  def toValue(): String = { "lucky" }
+  def toValue(): String = "lucky"
 }
